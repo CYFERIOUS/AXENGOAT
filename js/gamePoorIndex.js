fc0e3dd2a0f2af8dfec311c1d93f0cc54ab2a6cc
Fernando Javier Nossa Chaparro <FJNossaC@DANE.GOV.CO>
2017-09-04 17:52:39 -0500

    ready walk cycle

--- js/gamePoorIndex.js
+++ js/gamePoorIndex.js
@@ -3,7 +3,7 @@ var ModuleGame = (function () {
   var game = new Phaser.Game(950, 720, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    game.load.atlasJSONHash('bot', 'images/running_bot.png', 'images/running_bot.json');
+    game.load.spritesheet('robot', 'images/robot.png', 80, 111);
     game.load.image('casita', 'images/casita.png');
 
 }
@@ -12,54 +12,55 @@ var s;
 
 function create() {
 
-    s = game.add.sprite(game.world.centerX, game.world.centerY, 'bot');
-    s.anchor.setTo(0.5, 0.5);
-    s.scale.setTo(2, 2);
+      this.speed=2;
+       this.robot=game.add.sprite(game.world.centerX,330,"robot");
+       this.robot.anchor.set(0.5,0.5);
+       this.robot.animations.add('idle', [0,1,2,3,4,5,6,7,8,9], 12,true);
+       this.robot.animations.add('run', [10,11,12,13,14,15,16,17], 12,true);
+       this.robot.animations.play('idle');
 
      var c = game.add.sprite(game.world.centerX-256,210, 'casita');
 
-    s.animations.add('run');
-    s.animations.play('run', 10, true);
+    //s.animations.add('run');
+    //s.animations.play('run', 10, true);
 
 }
 
 function update() {
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
-        s.x -= 4;
-        if(s.x < 200){
-            s.x=200;
+        this.robot.x-=this.speed;
+        this.robot.play("run");
+        this.robot.scale.x=-1;
+        if(this.robot.x < 256){
+            this.robot.x = 256;
         }
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
-        s.x += 4;
-        if(s.x > 600){
-            s.x=600;
+        this.robot.x+=this.speed;
+        this.robot.play("run");
+        this.robot.scale.x=1;
+        if(this.robot.x > 710){
+          this.robot.x = 710;
         }
-    }
-
-    if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
-    {
-        s.y -= 4;
-    }
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-    {
-        s.y += 4;
+       
+    }else{
+      this.robot.play('idle');
     }
 
 }
 
 function render() {
-    game.debug.spriteInfo(s, 20, 32);
+    //game.debug.spriteInfo(s, 20, 32);
 
 }
  
  
    var publicMethod = function () {
-    
       render();
+      update();
   };
   
   return {
