607ff3b2531f1875429e1b0d3a8dc46b86f44263
Fernando Javier Nossa Chaparro <FJNossaC@DANE.GOV.CO>
2017-10-17 16:15:23 -0500

    fixed bug of unknown width, it was the width of barlife because the negative values

--- js/barLife.js
+++ js/barLife.js
@@ -1,7 +1,7 @@
 var BarLife = (function (Texto) {
 
      var actual = 0;
-     var score = 0;
+     var stamina = 0;
 
 
     var addBar = function(xpos){
@@ -25,21 +25,16 @@ var BarLife = (function (Texto) {
   };
 
     var lifeLevelAdd = function(value){        
-          actual = actual + value;
-            var stamina  =  actual;
+            actual = actual + value;
+            stamina  =  actual;
             this.myHealthBar.setPercent(stamina);
+            getBarPercent(stamina);
+            Texto.updateA("INDICE:"+stamina);
+            if(stamina <= 0){
+                Texto.updateA("INDICE:"+0);
+            }
            
-            score = stamina;
-
-            getBarPercent(score);
-            
-            if(score >= 0){
-              Texto.updateA("INDICE:"+score);
-            }
-            if(score <= 0){
-              Texto.updateA("0");
-              this.myHealthBar.setPercent(0);
-            }
+           
             if(actual < 33){
               Texto.updateB("SOBRESALIENDO",60);
               this.myHealthBar.setBarColor('#70FF01');
@@ -47,7 +42,7 @@ var BarLife = (function (Texto) {
                   Texto.updateB("PELIGRO: ZONA DE ESCASEZ",30);
                   this.myHealthBar.setBarColor('#E50000');
             }
-            if( score >= 100){
+            if( stamina >= 100){
               Texto.updateA("100 :( ");
               Texto.updateB("GAME OVER",60);
               setTimeout(function(){
@@ -58,21 +53,16 @@ var BarLife = (function (Texto) {
             }
 
             Texto.updateC("stage 1");
-
-
-    
     }
 
-    var lifeLevelMinus = function(value){
-        console.log("collide");
+    var lifeLevelMinus = function(value){        
+        actual = actual - value;
+        stamina = actual <= 0 ? 0 : actual;
+        this.myHealthBar.setPercent(stamina);
+        if(stamina <= 0){
+          Texto.updateA("INDICE:"+0);
+        }
         
-          actual = actual - value;
-            var stamina  =  actual;
-            this.myHealthBar.setPercent(stamina);
-            if(stamina == 0){
-                Texto.updateB("NOT POOR");
-            }
-    
     }
 
     var initLife = function(){
@@ -81,10 +71,10 @@ var BarLife = (function (Texto) {
 
     var getBarPercent = function(score){
       
-      if(score >= 33){
+      if(stamina >= 33){
         HomeLevel("casita2");
       }
-      if(score >= 66){
+      if(stamina >= 66){
         HomeLevel("casita3");
       }
       
