45b1c1aca11a803767f9c4c9934528b38c6644af
Fernando Javier Nossa Chaparro <FJNossaC@DANE.GOV.CO>
2018-02-14 18:52:29 -0500

    earthquake effect

--- js/characters.js
+++ js/characters.js
@@ -9,19 +9,18 @@
         game.load.spritesheet('son', 'images/robot3.png', 80, 111);
         game.load.spritesheet('daughter', 'images/robot4.png', 80, 111);
         game.load.spritesheet('aim', 'images/aim.png', 100, 100);
+        game.load.spritesheet('power', 'images/aim.png', 100, 100);
     };
 
     var mainCharacter = function (bot) {
 
       this.speed=10;
       this.robot=game.add.sprite(game.world.centerX,130,bot);
-
       game.physics.arcade.enable(this.robot);
       this.robot.anchor.set(0.8,0.8);
       this.robot.animations.add('idle', [0,1,2,3,4,5,6,7,8,9], 12,true);
       this.robot.animations.add('run', [10,11,12,13,14,15,16], 12,true);
       this.robot.animations.add('turbo', [20,21,22,23,24,25,26,27,28], 12,true);
-      
       this.robot.animations.play('idle',[0,1,2,3],12,true); 
       this.robot.body.collideWorldBounds = true;
       this.robot.body.bounce.y = 0.2;
@@ -37,7 +36,6 @@
         }).mouseup(function(){
             $("#body").css( 'cursor', 'cell' );
       });
-
     };
 
     var movements = function () {
@@ -64,13 +62,14 @@
         jumpTimer = game.time.now + 750;
       }
       if (game.input.mousePointer.isDown){
-        this.robot.animations.play('turbo',12,true);
+        this.robot.animations.play("turbo");
         game.physics.arcade.moveToPointer(this.robot, 700);
           if(game.input.mousePointer.x>=600){
             this.robot.scale.x=1;
           }else{
             this.robot.scale.x=-1;
           }
+         
       }
       if (game.input.mousePointer.isUp){
          this.robot.body.velocity.x = 0;
@@ -84,6 +83,7 @@
         emitter1.forEachAlive(function(particle){
           particle.body.acceleration.x = -80;
           particle.body.acceleration.y = 30;
+      
         }); 
       }
        
@@ -94,6 +94,7 @@
           emitter2.forEachAlive(function(particle){
             particle.body.acceleration.x = 80;
             particle.body.acceleration.y = 30;
+            
           });
        }
       
