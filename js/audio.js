ebebd85a84109dcdbae29f1c6a78eb00c1a756cd
Fernando Javier Nossa Chaparro <FJNossaC@DANE.GOV.CO>
2018-02-20 17:02:38 -0500

    sound way is going

---
+++ js/audio.js
@@ -0,0 +1,29 @@
+  var Audios = (function () {
+
+   var music;
+    
+    var preload = function () {
+        // game.load.audio('boden', ['sounds/audio1.mp3', 'assets/audio/bodenstaendig_2000_in_rock_4bit.ogg']);
+
+    };
+
+
+
+    var create = function () {
+
+    //  music = game.sound.play('boden');
+
+    
+    };
+
+    
+  
+  
+    return {
+      preloadMenu: preload,
+      createMenu: create
+    };
+
+  })();
+
+
