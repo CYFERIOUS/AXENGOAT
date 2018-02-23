f5c7a09c0732341eb4f9bb5c738336363b38d3c3
Fernando Javier Nossa Chaparro <FJNossaC@DANE.GOV.CO>
2018-02-23 14:53:48 -0500

    music for stage ok

--- js/audio.js
+++ js/audio.js
@@ -8,8 +8,7 @@
    var turbo;
     
     var preload = function () {
-        game.load.audio('boden', ['../sounds/hitHouse.mp3', '../sounds/hitHouse.ogg']);
-        game.load.audio('mistery', ['../sounds/hitMistery.mp3', '../sounds/hitMistery.ogg']);
+        game.load.audio('hitfloor', ['../sounds/hitHouse.mp3', '../sounds/hitHouse.ogg']);
         game.load.audio('stage', ['../sounds/audio3.mp3', '../sounds/audio3.ogg']);
         game.load.audio('robot', ['../sounds/pop.mp3', '../sounds/pop.ogg']);
         game.load.audio('platform', ['../sounds/plats.mp3', '../sounds/plats.ogg']);
@@ -20,40 +19,33 @@
 
 
     var audioStage = function () {
-     
-      //music = new Sound(game, "stage", 10, true);
+      alert("carga");
+       music = game.sound.play('stage');
+       music.loopFull(0.6);
 
     };
 
     hitHouse = function(){
-      hithouse = game.sound.play('boden');
-      //hithouse = game.sound.play('mistery');
+      hithouse = game.sound.play('hitfloor');
     }
 
     hitRobot = function(){
       hitrobot = game.sound.play('robot');
-      
     }
 
     hitPlatform = function(){
       hitplatform = game.sound.play('platform');
-      
     }
 
     addTurbo = function(){
       turbo = game.sound.play('turbo');
-      
     }
 
      hitDog = function(){
       hitdog = game.sound.play('gaspar');
-      
     }
 
 
-    
-  
-  
     return {
       preloadAudio: preload,
       stageAudio: audioStage,
